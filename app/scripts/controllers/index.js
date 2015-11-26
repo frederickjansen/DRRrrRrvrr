c33088b37d72a1270396ee2b2fff716e4621bd61
Frederick Jansen <frederick.jansen@gmail.com>
2015-11-26 13:39:58 -0500

    Files are now displayed on index page

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -10,21 +10,28 @@
 angular.module('drr.controllers')
   .controller('IndexCtrl', ['$rootScope', '$window', 'GoogleDrive', function ($rootScope, $window, GoogleDrive) {
     this.label = "Authorize";
+    var that = this;
 
-    $window.initGapi = function() {
-      checkAuth(true);
+    $window.initGapi = function () {
+      checkAuth(true).then(listFiles);
     };
 
-    $rootScope.$on('$googleDrive:oauthClick', function() {
-      checkAuth(false);
+    $rootScope.$on('$googleDrive:oauthClick', function () {
+      checkAuth(false).then(listFiles);
     });
 
-    function checkAuth() {
-      GoogleDrive.checkAuth().then(function () {
-        GoogleDrive.listFiles().then(function (files) {
-          console.log(files);
-        });
-      });
+    function checkAuth(immediate) {
+      return GoogleDrive.checkAuth(immediate);
     }
+
+    function listFiles() {
+      GoogleDrive.listFiles().then(
+        function (files) {
+          that.files = files;
+        }, function (error) {
+          console.log('Something went wrong getting files', error);
+        });
+    }
+
     console.log('index.js controller')
   }]);
