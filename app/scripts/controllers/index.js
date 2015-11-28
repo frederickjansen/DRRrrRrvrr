ddee2efc76a4304c12f7ce1273e3a6adce64a7da
Frederick Jansen <frederick.jansen@gmail.com>
2015-11-28 02:08:12 -0500

    Navigating back now executes gapi again

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -11,12 +11,15 @@ angular.module('drr.controllers')
   .controller('IndexCtrl', ['$rootScope', '$window', 'GoogleDrive', function ($rootScope, $window, GoogleDrive) {
     this.label = "Authorize";
     var that = this;
+    var running = false;
 
     $window.initGapi = function () {
+      running = true;
       checkAuth(true).then(listFiles);
     };
 
     $rootScope.$on('$googleDrive:oauthClick', function () {
+      running = true;
       checkAuth(false).then(listFiles);
     });
 
@@ -28,9 +31,14 @@ angular.module('drr.controllers')
       GoogleDrive.listFiles().then(
         function (files) {
           that.files = files;
+          running = false;
         }, function (error) {
           console.log('Something went wrong getting files', error);
         });
+    }
+
+    if (!running && $window.gapiLoaded) {
+      $window.initGapi();
     }
 
     console.log('index.js controller')
