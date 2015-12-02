9244f7efa6ab69396ed80a40a840be50a30b6438
Frederick Jansen <frederick.jansen@gmail.com>
2015-12-02 01:28:41 -0500

    Added two tests for document controller

--- test/spec/controllers/document.js
+++ test/spec/controllers/document.js
@@ -5,17 +5,36 @@ describe('Controller: DocumentCtrl', function () {
   // load the controller's module
   beforeEach(module('drr'));
 
-  var DocumentCtrl, scope;
+  var DocumentCtrl,
+    GoogleDrive,
+    scope,
+    stateParams;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($rootScope, $controller) {
+  beforeEach(inject(function ($rootScope, $controller, $q) {
+    GoogleDrive = {
+      displayFile: function () {}
+    };
+    var deferred = $q.defer();
+    spyOn(GoogleDrive, 'displayFile').and.returnValue(deferred.promise);
+
     scope = $rootScope.$new();
+    stateParams = { id: 1 };
+
     DocumentCtrl = $controller('DocumentCtrl', {
-      $scope: scope
+      $scope: scope,
+      $stateParams: stateParams,
+      GoogleDrive: GoogleDrive
     });
   }));
 
-  it('dummy test', function () {
-    expect(1).toBe(1);
+  it('displayFile not called when gapi is undefined', function () {
+    expect(GoogleDrive.displayFile).not.toHaveBeenCalled();
+  });
+
+  it('displayFile called after gApiLoaded broadcast', function () {
+    scope.$broadcast('$index:gApiLoaded');
+    expect(GoogleDrive.displayFile).toHaveBeenCalled();
+    //done();
   });
 });
