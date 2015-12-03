748ec0171407552cc12d3029c792165e37ab5a58
Frederick Jansen <frederick.jansen@gmail.com>
2015-12-03 00:13:21 -0500

    Added test for index.js

--- test/spec/controllers/index.js
+++ test/spec/controllers/index.js
@@ -5,18 +5,55 @@ describe('Controller: IndexCtrl', function () {
   // load the controller's module
   beforeEach(module('drr'));
 
-  var IndexCtrl, scope;
+  var IndexCtrl,
+    GoogleDrive,
+    window,
+    scope;
 
   // Initialize the controller and a mock scope
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($rootScope, $controller) {
-    scope = $rootScope.$new();
-    IndexCtrl = $controller('IndexCtrl', {
-      $scope: scope
+  beforeEach(inject(function ($rootScope, $controller, $q) {
+    GoogleDrive = {
+      checkAuth: function() {}
+    };
+
+    window = {
+      initGapi: function () {}
+    };
+
+    scope = {
+      $broadcast: function () {}
+    };
+
+    spyOn(scope, '$broadcast').and.callThrough();
+
+    spyOn(window, 'initGapi').and.callThrough();
+
+    spyOn(GoogleDrive, 'checkAuth').and.callFake(function () {
+      var def = $q.defer();
+      def.resolve('file');
+      return def.promise;
     });
+
+    IndexCtrl = $controller('IndexCtrl', {
+      $scope: scope,
+      $window: window,
+      GoogleDrive: GoogleDrive
+    });
+
   }));
 
-  it('dummy test', function () {
-    expect(1).toBe(1);
+  describe('initGapi', function () {
+    beforeEach(inject(function($rootScope) {
+      window.initGapi();
+      $rootScope.$apply();
+    }));
+
+    it('should call checkAuth', function () {
+      expect(GoogleDrive.checkAuth).toHaveBeenCalledWith(true);
+    });
+
+    it('should trigger gApiLoaded broadcast', function () {
+      expect(scope.$broadcast).toHaveBeenCalledWith('$index:gApiLoaded')
+    });
   });
 });
