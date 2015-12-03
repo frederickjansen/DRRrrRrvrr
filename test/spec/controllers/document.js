f06de80560a128e77b5b2de6d664310cf4ea635c
Frederick Jansen <frederick.jansen@gmail.com>
2015-12-02 23:26:30 -0500

    Finished testing document controller

--- test/spec/controllers/document.js
+++ test/spec/controllers/document.js
@@ -7,34 +7,76 @@ describe('Controller: DocumentCtrl', function () {
 
   var DocumentCtrl,
     GoogleDrive,
+    ZombieTranslator,
     scope,
     stateParams;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($rootScope, $controller, $q) {
+
     GoogleDrive = {
-      displayFile: function () {}
+      displayFile: function() {}
     };
-    var deferred = $q.defer();
-    spyOn(GoogleDrive, 'displayFile').and.returnValue(deferred.promise);
+
+    ZombieTranslator = {
+      translate: function() {}
+    };
+
+    spyOn(GoogleDrive, 'displayFile').and.callFake(function () {
+      var def = $q.defer();
+      def.resolve('file');
+      return def.promise;
+    });
+    spyOn(ZombieTranslator, 'translate').and.callFake(function () {
+      var deferred = $q.defer();
+      deferred.resolve('translation');
+      return deferred.promise;
+    });
 
     scope = $rootScope.$new();
-    stateParams = { id: 1 };
+    stateParams = {id: 1};
 
     DocumentCtrl = $controller('DocumentCtrl', {
       $scope: scope,
       $stateParams: stateParams,
-      GoogleDrive: GoogleDrive
+      GoogleDrive: GoogleDrive,
+      ZombieTranslator: ZombieTranslator
     });
   }));
 
-  it('displayFile not called when gapi is undefined', function () {
-    expect(GoogleDrive.displayFile).not.toHaveBeenCalled();
+  describe('displayFile', function () {
+    it('should not be called when gapi is undefined', function () {
+      expect(GoogleDrive.displayFile).not.toHaveBeenCalled();
+    });
+
+    it('should be called after gApiLoaded broadcast', function () {
+      scope.$broadcast('$index:gApiLoaded');
+      expect(GoogleDrive.displayFile).toHaveBeenCalled();
+    });
+
+    it('should be called with state params', function () {
+      scope.$broadcast('$index:gApiLoaded');
+      expect(GoogleDrive.displayFile).toHaveBeenCalledWith(1);
+    });
   });
 
-  it('displayFile called after gApiLoaded broadcast', function () {
-    scope.$broadcast('$index:gApiLoaded');
-    expect(GoogleDrive.displayFile).toHaveBeenCalled();
-    //done();
+  describe('zombieTranslator', function () {
+    beforeEach(inject(function ($rootScope) {
+      scope.$broadcast('$index:gApiLoaded');
+      $rootScope.$apply();
+    }));
+
+    it('should be called', inject(function ($rootScope) {
+
+      expect(ZombieTranslator.translate).toHaveBeenCalled();
+    }));
+
+    it('should be called with file', inject(function ($rootScope) {
+      expect(ZombieTranslator.translate).toHaveBeenCalledWith('file');
+    }));
+
+    it('should set zombieTranslation', inject(function ($rootScope) {
+      expect(DocumentCtrl.zombieTranslation).toBe('translation');
+    }));
   });
 });
