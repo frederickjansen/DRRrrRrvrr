b444ae7a4128a9c68dc246133c8055c2119eea5c
Frederick Jansen <frederick.jansen@gmail.com>
2015-12-04 01:46:56 -0500

    Minor revision to oauth test

--- test/spec/directives/oauthButton.js
+++ test/spec/directives/oauthButton.js
@@ -1,23 +1,28 @@
-describe('Directive: oauthButton', function() {
+describe('Directive: oauthButton', function () {
   var compile,
-    scope;
+    scope,
+    element;
 
   // Load the myApp module, which contains the directive
   beforeEach(module('drr'));
 
   // Store references to $rootScope and $compile
   // so they are available to all tests in this describe block
-  beforeEach(inject(function($compile, $rootScope){
+  beforeEach(inject(function ($compile, $rootScope) {
     // The injector unwraps the underscores (_) from around the parameter names when matching
     compile = $compile;
     scope = $rootScope.$new();
   }));
 
-  it('Replaces the element with the appropriate content', function() {
+  beforeEach(function () {
     // Compile a piece of HTML containing the directive
-    scope.label = 'Authorize';
-    var element = compile("<drr-oath-button label='label'></drr-oath-button>")(scope);
+    element = compile("<drr-oath-button label='label'></drr-oath-button>")(scope);
     // fire all the watches, so the scope expression {{label}} will be evaluated
+    scope.$digest();
+  });
+
+  it('Replaces the element with the appropriate content', function () {
+    scope.label = 'Authorize';
     scope.$digest();
     // Check that the compiled element contains the templated content
     expect(element.html()).toContain("<button class=\"ng-binding\">Authorize</button>");
